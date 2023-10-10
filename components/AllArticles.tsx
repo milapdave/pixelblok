f7f0a895acd25f515a166c02501e33aaac715af4
Milap Dave <milap@softnweb,in>
2023-10-10 09:20:54 +0530

    update

--- components/AllArticles.tsx
+++ components/AllArticles.tsx
@@ -20,8 +20,22 @@ interface AllArticlesProps {
 }
 
 const AllArticles: React.FC<AllArticlesProps> = ({ blok }) => {
+  const [articles, setArticles] = useState<Article[]>([]);
+  const [tags, setTags] = useState<Article[]>([]);
+  const [page, setPage] = useState(1);
+  const [checkedTags, setCheckedTags] = useState<string>();
+  const [newDataLoaded, setNewDataLoaded] = useState(false);
+
   // Convert the string value to a number for items per page
   const ITEMS_PER_PAGE = parseInt(blok.show_posts, 10);
+
+  useEffect(() => {
+    getData(ITEMS_PER_PAGE, page);
+  }, [page, checkedTags]);
+
+  useEffect(() => {
+    fetchTags();
+  }, []);
 
   // Function to get grid column classes based on the selected option
   const getGridColumnClasses = (columnOption: string | undefined) => {
@@ -40,7 +54,7 @@ const AllArticles: React.FC<AllArticlesProps> = ({ blok }) => {
   const columnOption = blok.column_option || '3';
 
   // Function to fetch data based on the number of items per page
-  const getData = async (ITEMS_PER_PAGE: number) => {
+  const getData = async (ITEMS_PER_PAGE: number, page: number) => {
     // Get an instance of the Storyblok API
     const storyblokApi = getStoryblokApi();
 
@@ -49,33 +63,78 @@ const AllArticles: React.FC<AllArticlesProps> = ({ blok }) => {
       version: 'draft',
       starts_with: 'blog/', // Adjust the starts_with value as needed
       is_startpage: false,
+      sort_by: 'created_at:desc',
       per_page: ITEMS_PER_PAGE,
+      page: page,
+      with_tag: checkedTags || '', // Convert the selected tags array to a comma-separated string
     };
 
     // Fetch data from Storyblok
     const { data } = await storyblokApi.get(`cdn/stories`, sbParams);
 
-    return data.stories;
+    const updatedArticles = data.stories.map((article: any) => {
+      article.content.slug = article.slug;
+      return article.content;
+    });
+    // setData((prevArticles) => [...prevArticles, ...updatedArticles]);
+
+    setData(updatedArticles);
+
+    // return data.stories;
+  };
+
+  const fetchTags = async () => {
+    // Get an instance of the Storyblok API
+    const storyblokApi = getStoryblokApi();
+
+    // Define parameters for fetching data
+    const sbParams: ISbStoriesParams = {
+      version: 'draft',
+      starts_with: 'blog/',
+    };
+
+    // Fetch data from Storyblok
+    const { data } = await storyblokApi.get('cdn/tags', sbParams);
+    setTags(data.tags);
   };
 
   // State to store fetched data
   const [data, setData] = useState<Article[]>([]);
 
-  useEffect(() => {
-    getData(ITEMS_PER_PAGE).then((result) => {
-      // Set the slug property in the content object for each article
-      const updatedArticles = result.map((article: any) => {
-        article.content.slug = article.slug;
-        return article.content;
-      });
-      setData(updatedArticles);
-    });
-  }, [ITEMS_PER_PAGE]);
+  const handleNextClick = () => {
+    setPage((prevPage) => prevPage + 1);
+  };
 
+  const handleCheckboxChange = (tag: string) => {
+    setPage(1);
+    setCheckedTags(tag === 'All' ? undefined : tag);
+  };
   return (
-    <div className='py-10 px-5 lg:px-24'>
-      <div className='grid w-full gap-8 py-10 text-2xl'>
+    <div className='px-5 py-10 lg:px-24'>
+      <div className='flex w-full justify-between  py-10 text-2xl'>
         <h2>{blok.heading}</h2>
+        <div className='flex items-center gap-4'>
+          <div
+            className={`cursor-pointer text-sm ${
+              checkedTags === 'All' ? 'font-bold' : ''
+            }`}
+            onClick={() => handleCheckboxChange('All')}
+          >
+            All
+          </div>
+          {tags?.map((story: any, index: any) => (
+            <div key={index} className='flex'>
+              <div
+                className={`cursor-pointer text-sm ${
+                  checkedTags === story.name ? 'font-bold' : ''
+                }`}
+                onClick={() => handleCheckboxChange(story.name)}
+              >
+                {story.name}
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
       <div
         className={`grid w-full gap-8  md:grid-cols-3  ${getGridColumnClasses(
@@ -86,6 +145,14 @@ const AllArticles: React.FC<AllArticlesProps> = ({ blok }) => {
         {data.map((article) => (
           <ArticleTeaser article={article} key={article.slug} />
         ))}
+      </div>
+      <div className='pt-20 text-center'>
+        <button
+          className='inline-flex rounded-sm bg-dark px-6 py-3 text-base font-semibold text-white hover:bg-black'
+          onClick={handleNextClick}
+        >
+          More Articles
+        </button>
       </div>
     </div>
   );
